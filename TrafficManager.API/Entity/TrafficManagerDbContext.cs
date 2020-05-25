539c36b251a32f3a768b903e9c23c5e82f54184b
craig cheney <40227803@live.napier.ac.uk>
2020-05-25 01:07:13 +0100

    forst commit

---
+++ TrafficManager.API/Entity/TrafficManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TrafficManager.API.Models;
+
+namespace TrafficManager.Infrastructure
+{
+    class TrafficManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public TrafficManagerDbContext(DbContextOptions<TrafficManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ TrafficManager.API/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TrafficManager.API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
